ced5bd7bb03d2a61d700fbe85324ad75ca8d8479
Wahyu Fairuz <wahyufairuz05@gmail.com>
2026-09-24 12:05:20 +0700

    Push: Wahyu Pages

---
+++ lib/wahyu_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WahyuPage extends StatefulWidget {
+  const WahyuPage({super.key});
+
+  @override
+  State<WahyuPage> createState() => _WahyuPageState();
+}
+
+class _WahyuPageState extends State<WahyuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
